4ca834c62cc617600b6ea028b7197b095e183d2c
PersonBelowRocks <ulrikwithc@gmail.com>
2022-05-25 10:19:16 +0200

    crate aliases + module for runtime code

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+extern crate bevy_ecs as ecs;
 extern crate nalgebra as na;
+extern crate volume as vol;
 
 mod block;
 mod chunk;
---
+++ src/runtime/mod.rs
@@ -0,0 +1,3 @@
+//! This module contains code related to the generator server's runtime. This includes the ECS world(s), ECS schedule(s), and async runtime for netcode.
+
+// TODO: we should maybe have some kind of Runtime struct to store all the data and state for the runtime. We'll interact with this struct to add generators and stuff.
