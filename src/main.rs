645deeecb168e1548c0693ed9387950ace27be37
PersonBelowRocks <ulrikwithc@gmail.com>
2022-05-25 10:15:27 +0200

    Remove code to start over again

--- src/main.rs
+++ src/main.rs
@@ -2,21 +2,13 @@ extern crate nalgebra as na;
 
 mod block;
 mod chunk;
-mod generate;
-mod net;
 mod util;
-mod volume;
 
 #[tokio::main]
 async fn main() {
     env_logger::init();
 
-    let mut server = net::server::Server::new()
-        .with_version(net::protocol::ProtocolVersion::V1)
-        .with_compression_threshold(256);
-
-    server.bind("0.0.0.0:4321".parse().unwrap()).await.unwrap();
-    server.run().await.unwrap();
+    todo!()
 }
 
 #[cfg(test)]
