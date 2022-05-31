e262f11aaa7dd2722177162ea9bd560f2589b267
PersonBelowRocks <ulrikwithc@gmail.com>
2022-05-31 11:57:40 +0200

    update `Chunk` and `ChunkSection` to new `volume` version + add vectorspace enum `Spaces` for chunk indexing

--- src/chunk/basic.rs
+++ src/chunk/basic.rs
@@ -67,56 +67,135 @@ impl Chunk {
         self.sections.get_mut(chunk_section_idx)
     }
 
-    /// Chunkspace access
     #[inline]
-    fn cs_get<Idx: VolumeIdx>(&self, idx: Idx) -> Option<&<Self as Volume>::Item> {
-        let [x, y, z] = idx.array::<i64>()?;
-        self.ls_get([x, y - self.bounding_box().min()[1], z])
+    pub fn bounding_box(&self) -> BoundingBox {
+        self.bounding_box
+    }
+}
+
+impl<Idx: VolumeIdx> VolumeAccess<Idx> for Chunk {
+    #[inline]
+    fn get(this: &Self, idx: Idx) -> Option<&Self::Item> {
+        let [x, y, z] = idx.array::<i32>()?;
+
+        // This is the height/index of the chunk section containing the position provided.
+        let section_idx = y / CHUNK_SIZE;
+        // This is the position within that section corresponding to the position provided
+        // (i.e., in the section's localspace).
+        let sectionspace_y = y % CHUNK_SIZE;
+
+        let section = this.get_chunk_section(section_idx as usize)?;
+        section.get([x, sectionspace_y, z])
     }
 
-    /// Mutable chunkspace access
     #[inline]
-    fn cs_get_mut<Idx: VolumeIdx>(&mut self, idx: Idx) -> Option<&mut <Self as Volume>::Item> {
-        let [x, y, z] = idx.array::<i64>()?;
-        self.ls_get_mut([x, y - self.bounding_box().min()[1], z])
+    fn set(this: &mut Self, idx: Idx, item: Self::Item) {
+        if let Some([x, y, z]) = idx.array::<i32>() {
+            let section_idx = y / CHUNK_SIZE;
+            let sectionspace_y = y % CHUNK_SIZE;
+
+            if let Some(section) = this.get_mut_chunk_section(section_idx as usize) {
+                section.set([x, sectionspace_y, z], item);
+            }
+        }
+    }
+
+    #[inline]
+    fn swap(this: &mut Self, idx: Idx, item: Self::Item) -> Option<Self::Item> {
+        let [x, y, z] = idx.array::<i32>()?;
+        let section_idx = y / CHUNK_SIZE;
+        let sectionspace_y = y % CHUNK_SIZE;
+
+        let section = this.get_mut_chunk_section(section_idx as usize)?;
+        section.swap([x, sectionspace_y, z], item)
+    }
+
+    #[inline]
+    fn contains(this: &Self, idx: Idx) -> bool {
+        if let Some([x, y, z]) = idx.array::<usize>() {
+            (y / CHUNK_SIZE as usize) < this.sections.len()
+                && x < CHUNK_SIZE as usize
+                && z < CHUNK_SIZE as usize
+        } else {
+            false
+        }
+    }
+}
+
+impl<Idx: VolumeIdx> VolumeAccess<Spaces<Idx>> for Chunk {
+    fn get(this: &Self, idx: Spaces<Idx>) -> Option<&Self::Item> {
+        let ls_idx = idx.ls(this.bounding_box());
+        this.get(ls_idx)
+    }
+
+    fn set(this: &mut Self, idx: Spaces<Idx>, item: Self::Item) {
+        let ls_idx = idx.ls(this.bounding_box());
+        this.set(ls_idx, item);
+    }
+
+    fn swap(this: &mut Self, idx: Spaces<Idx>, item: Self::Item) -> Option<Self::Item> {
+        let ls_idx = idx.ls(this.bounding_box());
+        this.swap(ls_idx, item)
+    }
+
+    fn contains(this: &Self, idx: Spaces<Idx>) -> bool {
+        match idx {
+            Spaces::Ls(idx) => this.contains(idx),
+            Spaces::Cs(idx) => {
+                if let Some([x, y, z]) = idx.array::<i64>() {
+                    this.contains([x, y + this.bounding_box().min()[1], z])
+                } else {
+                    false
+                }
+            }
+            Spaces::Ws(idx) => {
+                // Our bounding box is in worldspace.
+                this.bounding_box().contains(idx)
+            }
+        }
+    }
+}
+
+#[derive(Clone, Copy)]
+pub enum Spaces<Idx: VolumeIdx> {
+    Ls(Idx),
+    Cs(Idx),
+    Ws(Idx),
+}
+
+impl<Idx: VolumeIdx> Spaces<Idx> {
+    #[inline]
+    fn ls(self, bb: BoundingBox) -> Idx {
+        match self {
+            Self::Ls(idx) => idx,
+            Self::Cs(idx) => {
+                let [x, y, z] = idx.array::<i64>().unwrap();
+                Idx::from_xyz(x, y - bb.min()[1], z)
+            }
+            Self::Ws(idx) => {
+                let wpos = na::Vector3::from(idx.array::<i64>().unwrap());
+                let bb_min = na::Vector3::from(bb.min());
+
+                let lpos = wpos - bb_min;
+                let [x, y, z]: [i64; 3] = lpos.into();
+                Idx::from_xyz(x, y, z)
+            }
+        }
+    }
+
+    #[inline]
+    fn cs(self, _bb: BoundingBox) -> Idx {
+        todo!()
+    }
+
+    #[inline]
+    fn ws(self, _bb: BoundingBox) -> Idx {
+        todo!()
     }
 }
 
 impl Volume for Chunk {
     type Item = BlockId;
-
-    #[inline]
-    fn ls_get<Idx: VolumeIdx>(&self, idx: Idx) -> Option<&Self::Item> {
-        let [x, y, z] = idx.array::<i32>()?;
-
-        // This is the height/index of the chunk section containing the position provided.
-        let section_idx = y / CHUNK_SIZE;
-        // This is the position within that section corresponding to the position provided
-        // (i.e., in the section's localspace).
-        let sectionspace_y = y % CHUNK_SIZE;
-
-        let section = self.get_chunk_section(section_idx as usize)?;
-        section.ls_get([x, sectionspace_y, z])
-    }
-
-    #[inline]
-    fn ls_get_mut<Idx: VolumeIdx>(&mut self, idx: Idx) -> Option<&mut Self::Item> {
-        let [x, y, z] = idx.array::<i32>()?;
-
-        // This is the height/index of the chunk section containing the position provided.
-        let section_idx = y / CHUNK_SIZE;
-        // This is the position within that section corresponding to the position provided
-        // (i.e., in the section's localspace).
-        let sectionspace_y = y % CHUNK_SIZE;
-
-        let section = self.get_mut_chunk_section(section_idx as usize)?;
-        section.ls_get_mut([x, sectionspace_y, z])
-    }
-
-    #[inline]
-    fn bounding_box(&self) -> BoundingBox {
-        self.bounding_box
-    }
 }
 
 #[cfg(test)]
@@ -129,9 +208,11 @@ mod tests {
 
         let mut chunk = Chunk::new(DEFAULT_ID, na::vector![2, 2], -64, 320);
 
-        assert_eq!(chunk.get(na::vector![3i32, 3, 8]), None);
+        assert_eq!(chunk.get(Spaces::Ws(na::vector![3i32, 3, 8])), None);
         assert_eq!(
-            chunk.get(na::vector![3i32, 3, 8] + (na::vector![2, 0, 2] * CHUNK_SIZE)),
+            chunk.get(Spaces::Ws(
+                na::vector![3i32, 3, 8] + (na::vector![2, 0, 2] * CHUNK_SIZE)
+            )),
             Some(&DEFAULT_ID)
         );
 
@@ -140,18 +221,20 @@ mod tests {
 
         assert_eq!(
             chunk.swap(
-                na::vector![3i32, 3, 8] + (na::vector![2, 0, 2] * CHUNK_SIZE),
+                Spaces::Ws(na::vector![3i32, 3, 8] + (na::vector![2, 0, 2] * CHUNK_SIZE)),
                 BlockId::new(42)
             ),
             Some(DEFAULT_ID)
         );
         assert_eq!(
-            chunk.get(na::vector![3i32, 3, 8] + (na::vector![2i32, 0, 2] * CHUNK_SIZE)),
+            chunk.get(Spaces::Ws(
+                na::vector![3i32, 3, 8] + (na::vector![2i32, 0, 2] * CHUNK_SIZE)
+            )),
             Some(&BlockId::new(42))
         );
 
         assert_eq!(
-            chunk.cs_get(na::vector![3i32, 3, 8]),
+            chunk.get(Spaces::Cs(na::vector![3i32, 3, 8])),
             Some(&BlockId::new(42))
         );
     }
--- src/chunk/section.rs
+++ src/chunk/section.rs
@@ -1,4 +1,4 @@
-use vol::prelude::*;
+use vol::builtins::*;
 
 use crate::block::BlockId;
 
@@ -10,7 +10,7 @@ type CubicVolume<const N: usize, T> = StackVolume<N, N, N, T>;
 type ChunkSectionStorage = CubicVolume<CHUNK_SIZE_USIZE, BlockId>;
 
 /// A 16x16x16 cube of voxels/blocks.
-#[derive(Debug)]
+// #[derive(Debug)]
 pub struct ChunkSection {
     default: BlockId,
     volume: Option<ChunkSectionStorage>,
@@ -46,56 +46,53 @@ impl ChunkSection {
     }
 }
 
+impl<Idx: VolumeIdx> VolumeAccess<Idx> for ChunkSection {
+    #[inline]
+    fn get(this: &Self, idx: Idx) -> Option<&Self::Item> {
+        match this.volume {
+            Some(ref v) => v.get(idx),
+            None => {
+                if this.contains(idx) {
+                    Some(&this.default)
+                } else {
+                    None
+                }
+            }
+        }
+    }
+
+    #[inline]
+    fn set(this: &mut Self, idx: Idx, item: Self::Item) {
+        if !this.is_initialized() {
+            this.initialize();
+            this.volume.as_mut().unwrap().set(idx, item);
+        } else {
+            this.volume.as_mut().unwrap().set(idx, item);
+        }
+    }
+
+    #[inline]
+    fn swap(this: &mut Self, idx: Idx, item: Self::Item) -> Option<Self::Item> {
+        if !this.is_initialized() {
+            this.initialize();
+            this.volume.as_mut().unwrap().swap(idx, item)
+        } else {
+            this.volume.as_mut().unwrap().swap(idx, item)
+        }
+    }
+
+    #[inline]
+    fn contains(_this: &Self, idx: Idx) -> bool {
+        if let Some([x, y, z]) = idx.array::<usize>() {
+            x < CHUNK_SIZE_USIZE && y < CHUNK_SIZE_USIZE && z < CHUNK_SIZE_USIZE
+        } else {
+            false
+        }
+    }
+}
+
 impl Volume for ChunkSection {
     type Item = BlockId;
-
-    #[inline]
-    fn ls_get<Idx: VolumeIdx>(&self, idx: Idx) -> Option<&Self::Item> {
-        match self.volume {
-            Some(ref vol) => vol.ls_get(idx),
-            None => {
-                if self.contains(idx) {
-                    Some(&self.default)
-                } else {
-                    None
-                }
-            }
-        }
-    }
-
-    #[inline]
-    fn ls_get_mut<Idx: VolumeIdx>(&mut self, idx: Idx) -> Option<&mut Self::Item> {
-        match self.volume {
-            Some(ref mut vol) => vol.ls_get_mut(idx),
-            None => {
-                if self.contains(idx) {
-                    // We're gonna initialize the section here for 2 reasons:
-                    // - We need to return a mutable reference which cannot be owned by the function, and returning a mutable reference
-                    //   to our own default BlockId (like Self::ls_get) would be incredibly dumb, because then the caller could mutate our default which could be
-                    //   reused in the future, leading to unpredictable behaviour and horrific bugs.
-                    //
-                    // - The caller may have called this function by calling Volume::swap, in which case they (probably) want to swap whatever is at this index.
-                    //   It's ergonomic and convenient for the caller if the section just initializes itself in that case, instead of panicking or
-                    //   returning something stupid (see above), eliminating the need for a bunch of safeguards and sanity checks.
-                    self.initialize();
-                    self.ls_get_mut(idx)
-                } else {
-                    None
-                }
-            }
-        }
-    }
-
-    #[inline]
-    fn bounding_box(&self) -> BoundingBox {
-        if let Some(ref vol) = self.volume {
-            // We'll grab the existing bounding box if it exists due to performance reasons.
-            // BoundingBox::new has some logic in it that we might wanna avoid running multiple times.
-            vol.bounding_box()
-        } else {
-            BoundingBox::new_origin(CHUNK_SECTION_CORNER.into())
-        }
-    }
 }
 
 #[cfg(test)]
